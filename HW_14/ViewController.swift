d08bbc5d6bb5a6e84de6a126516a6de9e5b3ca4c
Roman <vazhnik.roma@mail.ru>
2019-09-07 21:23:03 +0300

    Initial Commit

---
+++ HW_14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_14
+//
+//  Created by Роман Важник on 07/09/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
